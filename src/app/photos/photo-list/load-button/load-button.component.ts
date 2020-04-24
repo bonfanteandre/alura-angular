9328bc9fb85ca7ca239493908611a868da479ffa
André Bonfante <andre458bonfante@gmail.com>
2020-04-24 13:23:59 -0300

    Carregando mais imagens ao clicar

--- src/app/photos/photo-list/load-button/load-button.component.ts
+++ src/app/photos/photo-list/load-button/load-button.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-load-button',
+  selector: 'ap-load-button',
   templateUrl: './load-button.component.html',
   styleUrls: ['./load-button.component.css']
 })
 export class LoadButtonComponent implements OnInit {
+
+  @Input()
+  hasMore: boolean = false;
 
   constructor() { }
 
