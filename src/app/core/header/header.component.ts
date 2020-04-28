552efea4c98c2b77f7f87d5ce5642c5a134c5faa
André Bonfante <andre458bonfante@gmail.com>
2020-04-28 14:19:35 -0300

    Implementando logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { UserService } from "../user/user.service";
 import { User } from "../user/user";
@@ -12,8 +13,13 @@ export class HeaderComponent {
     user$: Observable<User>
     user: User;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService, private router: Router) {
         this.user$ = this.userService.getUser();
         this.user$.subscribe(user => this.user = user);
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['']);
     }
 }
